bc4a8d859feaf62e9a38d6ca1b64f68d57458edb
bansalnitin92 <bansalnitin92@gmail.com>
2019-11-28 14:39:44 +0530

    Create HelperMethods.java

---
+++ HelperMethods.java
@@ -0,0 +1,13 @@
+import java.util.UUID;
+
+public class HelperMethods {
+
+  /**
+   * function to generate UUID.
+   *
+   */
+
+  public String generateUuid() {
+    return UUID.randomUUID().toString();
+  }
+}
